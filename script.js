72c50047691ff13cacaed65ac635c894fa2eb377
nishb369 <nishbcodes@gmail.com>
2024-10-29 00:27:56 +0530

    sounds added

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const wordsArray = "apartment catch position imagine "
+const words = "apple orange banana grape peach lemon cherry plum pear mango kiwi melon fig date papaya guava olive corn rice wheat oats beans peas spinach kale onion carrot garlic pepper potato ginger radish tomato lettuce broccoli cucumber pumpkin cabbage celery avocado mushroom berry nut pasta bread milk cheese soup salad cake pie cookie muffin pizza burger sandwich taco nachos sushi fries coffee tea juice water soda wine beer steak chicken turkey pork beef bacon sausage tofu shrimp fish rice pasta stew curry waffle honey jelly butter egg cream yogurt basil thyme cinnamon cocoa mint sugar salt pepper vinegar oil butter toast bagel donut popcorn cereal pancake burrito nacho enchilada quesadilla lasagna spaghetti macaroni noodle meatball chili casserole smoothie wrap stirfry toast quiche pretzel dumpling gnocchi "
+
+let wordsArray =  ""
+let n = 10; 
+for (let i = 0; i < n; i++) {
+    let randomIndex = Math.floor(Math.random() * 100);
+    wordsArray+=words.split(" ")[randomIndex]+" "
+}
+
 var disp_area = document.querySelector(".text_area")
 for( i=0; i<wordsArray.length; i++){
     if (wordsArray[i]!=' ')
@@ -8,6 +16,9 @@ for( i=0; i<wordsArray.length; i++){
 }
 
 var textInput = document.querySelector("#textInput")
+var key_sound = document.querySelector("#key_sound")
+var enter_sound = document.querySelector("#enter_sound")
+var lvl_complete = document.querySelector("#lvl_complete")
 
 corr = 0
 wrong = 0
@@ -21,6 +32,9 @@ textInput.addEventListener("mouseenter",function(){
 })
 
 textInput.addEventListener("keydown",function(){
+
+    key_sound.play()
+    key_sound.playbackRate = 2.5
 
     event.key==" "?keypressedid="#space":keypressedid='#'+String(event.key)
     keyto = document.querySelector(keypressedid)
@@ -43,6 +57,7 @@ textInput.addEventListener("keydown",function(){
         document.querySelector("body").innerHTML+=`<div><h1 style="color:white; font-family:Helvetica">Accuracy:${Math.round(corr/(corr+wrong)*100)}%</h1></div>`
         timeend=Date.now()
         console.log(timeend,timestart,(timeend-timestart)/1000);
+
     } 
 })
 
@@ -52,4 +67,5 @@ textInput.addEventListener("keyup",function(){
     keyto.style.transform="translateY(0px)"
     keyto.style.backgroundColor="#f7f7f7"
 })
+
 
